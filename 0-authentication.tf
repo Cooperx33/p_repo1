0e12efce35919e750e67add5ebeaceae3caab313
cooperx33 <cooperx33@gmail.com>
2025-05-26 21:40:17 -0500

    auth

---
+++ 0-authentication.tf
@@ -0,0 +1,6 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "coops-custom-class65"
+  region  = "us-central1"
+  credentials = "coops-custom-class65-a9bfc7317d0d.json"
+}
