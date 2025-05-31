dc18e846acdf05fc94af4dd31038886437ff1231
cooperx33 <cooperx33@gmail.com>
2025-05-30 23:52:17 -0500

    router

--- 4-router.tf
+++ 4-router.tf
@@ -1,6 +1,12 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
-resource "google_compute_router" "router" {
-  name    = "myrouter"
+resource "google_compute_router" "iowarouter" {
+  name    = "iowarouter"
   region  = "us-central1"
+  network = google_compute_network.my-vpc.id
+}
+
+resource "google_compute_router" "osakarouter" {
+  name    = "osakarouter"
+  region  = "asia-northeast2"
   network = google_compute_network.my-vpc.id
 }
